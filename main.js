c6ae01b0145020033db7d552ae78f77c82cc5a75
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-02-26 20:41:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var links = document.getElementsByClassName('links');
+
+function loadPage(pn){
+    $('.links').removeClass('selected');
+    links[pn-1].classList.add('selected');
+    if(pn==3) { $('#main').load('stopwatch.html');}
+    if(pn==2) { $('#main').load('tipCalculator.html');}
+    if(pn==1) { $('#main').load('simpleCalculator.html');}
+    if(pn==4) $('#main').load('colorChange.html');
+    if(pn==5) $('#main').load('randomCard.html', randomCardSetup);
+    if(pn==6) { $('#main').load('recipeApp.html', recipeSetup);}
+}
+
 
